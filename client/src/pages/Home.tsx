9d544680a30bd2fc8fd5b1db50e449e826cf7444
Mateo <mateosanchezcr@gmail.com>
2026-05-21 21:52:14 -0300

    home

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const home = () => {
+  return (
+    <div className="container">
+      <div className="container">
+        <h1>Home</h1>
+        <p>This is the home page.</p>
+      </div>
+    </div>
+  )
+}
+
+export default home
